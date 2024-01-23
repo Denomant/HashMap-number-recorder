3ea0a04c3ffe92395e42199f2a6cbe99dc07ab01
Denomant <denomant.yt@gmail.com>
2024-01-23 19:51:23 +0200

    Simple hash function

--- Source/HashMapLogic.h
+++ Source/HashMapLogic.h
@@ -1,5 +1,20 @@
+#include <string>
+
+using namespace std;
+
+int Hash(string StringToHash) {
+	// Summs the ASCII values in square of every char, and modulus it by 100
+
+	int Summ = 0;
+	for(char i : StringToHash) {
+		Summ += int(i) *int(i);
+	}
+	
+	return Summ % 100;
+}
 
 class HashMap {
+	
 
 
 };
