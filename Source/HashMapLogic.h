a11a7ef88cfcf57a9f9dafb7d2e41452f5b93c8a
Denomant <denomant.yt@gmail.com>
2024-01-24 21:47:48 +0200

    Initialize local vars and a destructor

--- Source/HashMapLogic.h
+++ Source/HashMapLogic.h
@@ -2,19 +2,30 @@
 
 using namespace std;
 
-int Hash(string StringToHash) {
-	// Summs the ASCII values in square of every char, and modulus it by 100
-
-	int Summ = 0;
+int Hash(string StringToHash, int HashRange) {
+	// Summs the ASCII values in square of every char, and modulus it by HashRange 
+	int Sum = 0;
 	for(char i : StringToHash) {
-		Summ += int(i) *int(i);
+		Sum += int(i) *int(i);
 	}
 	
-	return Summ % 100;
+	return Sum % HashRange;
 }
 
 class HashMap {
-	
+	/*
+	Similar to set in Python, in functionality
+	Solves collisions using the linear method
+	*/
 
+	// initialize local vars
+protected:
+	int MapLen = 16;//First len
+	int *MapArray = new int[MapLen]();// () means set every value as 0
+
+public:
+	~HashMap() {
+		delete[] MapArray;
+	}
 
 };
